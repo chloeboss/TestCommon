95e7b1d0378681428cd55ae7d005232861ad6da8
ChloeZeng <chlo.zeng@gmail.com>
2020-10-14 00:01:23 +1300

    add .net standard project

---
+++ TestCommon2.0/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestCommon2._0
+{
+    public class Class1
+    {
+        public class Logger
+        {
+            public void Log(string text)
+            {
+                Console.Write(text);
+            }
+        }
+    }
+}
